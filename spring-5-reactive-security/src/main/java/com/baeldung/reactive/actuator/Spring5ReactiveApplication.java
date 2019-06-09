740483f85836686ecafc912e0719480c886ab43a
Vishal Pawar <vishal.pawar@vishal-pawar.jetsynthesys.net>
2019-06-09 10:05:57 +0530

    first commit

---
+++ spring-5-reactive-security/src/main/java/com/baeldung/reactive/actuator/Spring5ReactiveApplication.java
@@ -0,0 +1,14 @@
+package com.baeldung.reactive.actuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5ReactiveApplication{
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring5ReactiveApplication.class, args);
+        System.out.println("Hello World");
+    }
+
+}
